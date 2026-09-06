d2021aeff70c9780c1e61400080a0c7f1a5d5da8
Herbert Joseph <techniquea2z@gmail.com>
2026-09-06 16:02:52 -0400

    fix(og): add meta tags + opengraph + copy

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,7 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  /** Minimum supported iOS version shown on /download. Defaults to "14" if unset. */
+  readonly IOS_MIN_VERSION?: string;
+}
